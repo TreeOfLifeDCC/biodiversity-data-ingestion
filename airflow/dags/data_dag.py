98a6771d51d08f8c6076d877ce10167039b4d2c9
Alexey Sokolov <alexey@ebi.ac.uk>
2025-02-21 11:12:14 +0000

    Updating dag to ingest all available data

--- airflow/dags/data_dag.py
+++ airflow/dags/data_dag.py
@@ -10,9 +10,9 @@ from airflow.io.path import ObjectStoragePath
 
 
 @task
-def ingest_gtf(url: str) -> None:
+def ingest_gtf(url: str, file_id: str) -> None:
     base = ObjectStoragePath(
-        "gs://google_cloud_default@prj-ext-prod-biodiv-data-in-gbdp")
+        "gs://google_cloud_default@prj-ext-prod-biodiv-data-in-gbdp/gtf_files")
     base.mkdir(exist_ok=True)
     gca_accession = url.split("/")[7]
     path = base / f"{gca_accession}.jsonl"
@@ -45,8 +45,25 @@ def biodiversity_annotations_ingestion():
     """
     This DAG downloads GTF files, format them into json files and upload to GCS
     """
-    ingest_gtf.override(task_id=f"test_ingest_gtf")(
-        "https://ftp.ensembl.org/pub/rapid-release/species/Bathymodiolus_brooksi/GCA_963680875.1/ensembl/geneset/2024_01/Bathymodiolus_brooksi-GCA_963680875.1-2024_01-genes.gtf.gz")
+    offset = 0
+    gbdp_response = requests.get(
+        f"https://www.ebi.ac.uk/biodiversity/api/data_portal?limit=1000&offset={offset}"
+        f"&filter=annotation_complete:Done").json()
+    results_length = len(gbdp_response["results"])
+    annotations_data = gbdp_response["results"]
+    while results_length != 0:
+        offset += 1000
+        gbdp_response = requests.get(
+            f"https://www.ebi.ac.uk/biodiversity/api/data_portal?limit=1000&offset="
+            f"{offset}&filter=annotation_complete:Done").json()
+        results_length = len(gbdp_response["results"])
+        annotations_data.extend(gbdp_response["results"])
+    for record in annotations_data:
+        for annotation in record["_source"]["annotation"]:
+            url = annotation["annotation"]["GTF"]
+            ingestion_id = url.split("/")[-1]
+            ingest_gtf.override(task_id=f"{ingestion_id}_ingest_gtf")(
+                url=url, ingestion_id=ingestion_id)
 
 
 biodiversity_annotations_ingestion()
