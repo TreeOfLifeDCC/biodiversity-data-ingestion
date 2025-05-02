e7a92f5f683b82b942a17a39ce914d08b5f2e583
Alexey Sokolov <alexey@ebi.ac.uk>
2025-05-02 14:06:50 +0100

    Updating image version

--- airflow/dags/dependencies/common_functions.py
+++ airflow/dags/dependencies/common_functions.py
@@ -8,7 +8,7 @@ def start_apache_beam(biodiversity_project_name):
     region = "europe-west2"
     body = {
         "launchParameter": {
-            "jobName": "biodiversity-ingestion-2025-05-01",
+            "jobName": "biodiversity-ingestion-2025-05-02",
             "parameters": {
                 "input_path": f"gs://{gc_project_name}-"
                 f"{biodiversity_project_name}/*jsonl",
@@ -24,10 +24,10 @@ def start_apache_beam(biodiversity_project_name):
                 "153439618737/staging",
                 "sdkContainerImage": f"{region}-docker.pkg.dev/"
                 f"{gc_project_name}/apache-beam-pipelines/"
-                f"biodiversity_etl:20250501-090627",
+                f"biodiversity_etl:20250502-112717",
             },
             "containerSpecGcsPath": f"gs://{gc_project_name}_cloudbuild/"
-            f"biodiversity_etl-20250501-090627.json",
+            f"biodiversity_etl-20250502-112717.json",
         }
     }
     return DataflowStartFlexTemplateOperator(
