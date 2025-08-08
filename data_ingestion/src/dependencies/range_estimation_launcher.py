ce0fd74e610550023d0b38b8b564a64c42241503
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2025-08-08 10:01:54 +0100

    Updating launcher to make BigQuery args optional.

--- data_ingestion/src/dependencies/range_estimation_launcher.py
+++ data_ingestion/src/dependencies/range_estimation_launcher.py
@@ -9,9 +9,9 @@ def run(argv):
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--input_glob", required=True, help="Glob pattern to cleaned occurrences .jsonl files")
-    parser.add_argument("--bq_table", required=True, help="BigQuery table name: project.dataset.table")
-    parser.add_argument("--bq_schema", required=True, help="Path to BigQuery schema JSON file")
-    parser.add_argument("--temp_location", required=True, help="GCS path for temporary BQ file loads")
+    parser.add_argument("--bq_table", required=False, help="BigQuery table name: project.dataset.table")
+    parser.add_argument("--bq_schema", required=False, help="Path to BigQuery schema JSON file")
+    parser.add_argument("--temp_location", required=False, help="GCS path for temporary BQ file loads")
 
     args, beam_args = parser.parse_known_args(argv)
     range_estimation_pipeline(args, beam_args)
