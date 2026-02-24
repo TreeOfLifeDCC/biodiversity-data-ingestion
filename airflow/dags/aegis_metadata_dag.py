dd316ef9b06bb93e5e25af1e6e159d5db7cb614d
yroochun <yroochun@ebi.ac.uk>
2026-02-24 15:16:29 +0000

    updated aegis dag

--- airflow/dags/aegis_metadata_dag.py
+++ airflow/dags/aegis_metadata_dag.py
@@ -11,6 +11,7 @@ from dependencies.common_functions import start_aegis_beam
 @task
 def get_aegis_metadata(study_id: str, bucket_name: str) -> None:
     from dependencies import collect_metadata_experiments_assemblies
+    from google.cloud import storage
 
     metadata = collect_metadata_experiments_assemblies.main(
         study_id,
@@ -18,14 +19,15 @@ def get_aegis_metadata(study_id: str, bucket_name: str) -> None:
         bucket_name
     )
 
-    base = ObjectStoragePath(f"gs://google_cloud_default@{bucket_name}")
-    base.mkdir(exist_ok=True)
-    path = base / f"{study_id}.jsonl"
+    client = storage.Client(project="prj-ext-prod-biodiv-data-in")
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(f"{study_id}.jsonl")
 
-    with path.open("w") as file:
-        for sample_id, record in metadata.items():
-            file.write(f"{json.dumps(record)}\n")
+    content = ""
+    for sample_id, record in metadata.items():
+        content += f"{json.dumps(record)}\n"
 
+    blob.upload_from_string(content, content_type="application/json")
 
 @task
 def trigger_aegis_beam_pipeline(bucket_name: str) -> None:
@@ -39,11 +41,6 @@ def trigger_aegis_beam_pipeline(bucket_name: str) -> None:
     tags=["aegis_metadata_ingestion"],
 )
 def aegis_metadata_ingestion():
-    """
-    This DAG pulls data from ENA (PRJEB80366) and BioSamples and builds
-    Elasticsearch indices for AEGIS project
-    """
-
     aegis_config = aegis_projects["PRJEB80366"]
     study_id = "PRJEB80366"
     bucket_name = aegis_config["bucket_name"]
