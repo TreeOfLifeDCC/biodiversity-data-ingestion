5ef8b414ec1934d62c0638be5684fe58223b7b55
yroochun <yroochun@ebi.ac.uk>
2026-08-05 09:48:47 +0100

    cron and config changes

--- airflow/deployment/create-service/main.py
+++ airflow/deployment/create-service/main.py
@@ -77,8 +77,8 @@ def start_composer_environment(http_request):
     )
 
     worker = WorkloadsConfig.WorkerResource(
-        cpu=_float_env("WORKER_CPU", 1.0),
-        memory_gb=_float_env("WORKER_MEM_GB", 4.0),
+        cpu=_float_env("WORKER_CPU", 2.0),
+        memory_gb=_float_env("WORKER_MEM_GB", 8.0),
         storage_gb=_float_env("WORKER_STORAGE_GB", 10.0),
         min_count=_int_env("WORKER_MIN_COUNT", 1),
         max_count=_int_env("WORKER_MAX_COUNT", 10),
